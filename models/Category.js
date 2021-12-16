285416043aed5a8c53648955a9f0675869de396b
pardue95 <85760640+pardue95@users.noreply.github.com>
2021-12-16 13:26:09 -0600

    added models

--- models/Category.js
+++ models/Category.js
@@ -4,9 +4,19 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+//setup fields for Category model
 Category.init(
   {
-    // define columns
+  id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
@@ -18,3 +28,4 @@ Category.init(
 );
 
 module.exports = Category;
+
